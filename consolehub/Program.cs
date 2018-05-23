5b0562fda24567cd82c7766bb1efb53376937eb0
Gonzalez Vazquez <francisco.gonzalez@external.wolterskluwer.com>
2018-05-23 14:41:39 +0200

    Agregar archivos de proyecto.

---
+++ consolehub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolehub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
